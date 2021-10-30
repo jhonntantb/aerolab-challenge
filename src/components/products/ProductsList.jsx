09ee11a0148d4736ed50c28988be6e1f975dba1c
jhonntantb <jhonntantb@gmail.com>
2021-10-30 15:25:41 -0500

    sort products

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -8,7 +8,9 @@ function ProductsList() {
     const dispatch = useDispatch();
 
     const [productsView,setProductsView]=useState([])
-    const [cardsPerPage,setCardsPerPage]=useState(16)
+    const [cardPerPage,setCardPerPage]=useState(16)
+    const [currentPage,setCurrentPage]=useState(1)
+    const [sortPrice,setSortPrice]=useState("")
 
     const products=useSelector(state=>state.products)
     const total=products.length
@@ -19,18 +21,39 @@ function ProductsList() {
     useEffect(() => {
         products.length>0&&setProductsView(products)
     }, [products])
+
+    if(sortPrice.length>0){
+        if(sortPrice==="increment"){
+            const orderProducts=products.sort((a,b)=>a.cost-b.cost)
+        }
+        if(sortPrice==="decrement"){
+            const orderProducts=products.sort((a,b)=>b.cost-a.cost)
+        }
+        if(sortPrice==="mostRecent"){
+            dispatch(getAllProducts())
+        }
+        
+    }
+
+    const handleSortPrice=(e)=>{
+        setSortPrice(e.target.value)
+    }
+    const indexOfLastItem=currentPage*cardPerPage;//16
+    const indexOfFirstItem=indexOfLastItem-cardPerPage;//0
+    let currentItems= productsView.slice(indexOfFirstItem,indexOfLastItem);
+
     return (
         <div>
             <div className="sort">
                 <p>{"16 of "}{total} products</p>
                 <p>|</p>
                 <p>Sort by:</p>
-                <button>Most recent</button>
-                <button>Lowets price</button>
-                <button>Highest price</button>
+                <button value="mostRecent" onClick={e=>handleSortPrice(e)} >Most recent</button>
+                <button value="increment" onClick={e=>handleSortPrice(e)} >Lowets price</button>
+                <button value="decrement" onClick={e=>handleSortPrice(e)} >Highest price</button>
             </div>
-            <div>
-            {productsView.length>0&&productsView.map(e=><CardProduct 
+            <div className="productList">
+            {productsView.length>0&&currentItems.map(e=><CardProduct 
             image={e.img.url}  
             name={e.name} 
             cost={e.cost} 
